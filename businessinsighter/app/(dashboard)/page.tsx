74d1b64639d44e942ad70895bae040010b926190
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-08-26 19:38:20 +0530

    Design Header Component

--- businessinsighter/app/(dashboard)/page.tsx
+++ businessinsighter/app/(dashboard)/page.tsx
@@ -2,9 +2,10 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
-      <p>This is an authenticated route</p>
-      <UserButton afterSignOutUrl="/" />
-    </>
+
+    <div>
+      DashboardPage
+    </div>
+    
   );
 }
